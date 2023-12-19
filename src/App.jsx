5ac6f0812790cba9ad1563f8004899358c6c27dd
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-19 18:22:15 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = ()=>{
+  return(
+    <div className={`text-5xl -bg--color-primary shadow-light`}>Hello World!!</div>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "--color-primary":"#eb2f64",
+        '--color-primary-light':"#FF3366",
+        '--color-primary-dark': "#BA265D",
+        '--color-grey-light-1': "#faf9f9",
+        '--color-grey-light-2': "#f4f2f2",
+        '--color-grey-light-3': "#f0eeee",
+        '--color-grey-light-4':' #ccc',
+        '--color-grey-dark-1': '#333',
+        '--color-grey-dark-2': '#777',
+        '--color-grey-dark-3': '#999',
+      },
+      boxShadow:{
+        'dark':'0 2rem 6rem rgba(0,0,0,.3)',
+        'light':'0 2rem 5rem rgba(0,0,0,.06)',
+      }
+    },
+  },
+  plugins: [],
+}
+
